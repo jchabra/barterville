e61f552baca44957aab415dcbe2b674f3206df17
Jasmine Chabra <jchabra@gmail.com>
2012-11-26 15:55:22 -0500

    controllers

---
+++ app/controllers/notification_controller.rb
@@ -0,0 +1,10 @@
+class NotificationController < ApplicationController
+	def index
+	end
+	def new
+	end
+	def show
+	end
+	def create
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121126202525) do
+ActiveRecord::Schema.define(:version => 20121126204748) do
 
   create_table "notifications", :force => true do |t|
     t.string   "subject"
     t.text     "message"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "products", :force => true do |t|
@@ -27,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20121126202525) do
     t.string   "condition"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
