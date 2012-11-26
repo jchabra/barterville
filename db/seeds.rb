e8f6cae023159236507665c6f137b394105f7253
Jasmine Chabra <jchabra@gmail.com>
2012-11-26 16:23:07 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+Product.delete_all
+
+zip = [11111, 11112, 11113, 11114, 11115]
+
+('A'..'Z').each do |letter|
+	u = User.create(:username => letter, :first_name => "first#{letter}", :last_name => "last#{letter}", :password => '123', :password_confirmation => '123', :zip => zip.sample, :bio => 2.sentences)
+end
+
+condition = ['new', 'used', 'poor']
+
+('A'..'Z').each do |letter|
+	p = Product.create(:name => letter, :description => 2.sentences, :condition => condition.sample, :user_id => User.all.sample.id)
+end
