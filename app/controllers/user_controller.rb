fe92cd1a52da66e9f2950cd226103165bdd352e9
Jasmine Chabra <jchabra@gmail.com>
2012-11-26 16:36:34 -0500

    users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,14 +1,23 @@
 class UserController < ApplicationController
 	def index
+		users = User.all
 	end
 	def new
+		user = User.new
 	end
 	def create
+		@user = User.new(params[:user])
+	    @user.username = @user.username.downcase
+	    @user.save
 	end
 	def show
+		user_id = params[:id]
 	end
 	def edit
+		user = User.find(params[:id])
 	end
 	def destroy
+		user = User.find(params[:id])
+    	user.delete
 	end
 end
