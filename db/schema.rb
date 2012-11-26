e911f7ed6a57225e589c5e89cb751c730edfd3bd
Dustin Coates <dustin@dcoates.com>
2012-11-26 15:55:41 -0500

    Preparing for pull

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121126202525) do
+ActiveRecord::Schema.define(:version => 20121126203357) do
 
   create_table "notifications", :force => true do |t|
     t.string   "subject"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20121126202525) do
     t.string   "condition"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
